eaf22d33b01359c0f1e609a25b3d1fd5f89d136d
3d-tours <134265032+3d-tours@users.noreply.github.com>
2026-08-07 18:26:04 +0300

    3

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 5.0.1/15068
 // Filename: ???????0.ggsk
-// Generated Пт авг 7 16:47:09 2026
+// Generated Пт авг 7 18:24:09 2026
 
 function pano2vrSkin(player,base) {
 	var ggSkinVars = [];
